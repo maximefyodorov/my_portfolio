bc82fc4155a511f3dee58bd197b70ba398b3d281
maximefyodorov <maxime.fyodorov@gmail.com>
2020-09-25 12:52:48 +0300

    Minor script improvements

--- templates/mastering-portfolio.py
+++ templates/mastering-portfolio.py
@@ -2,38 +2,37 @@ import sqlite3
 import jinja2
 from collections import namedtuple
 
+def data_dict(input, name, list):
+  result = []
+  for item in input:
+    current = namedtuple(name, list)
+    result.append(current(*item)._asdict())
+  return result
+
 db = sqlite3.connect("portfolio-data.db3")
 crsr = db.cursor()
 
 crsr.execute("SELECT typename, intro_text, abbrev, icon FROM TYPES")
-genres_raw = crsr.fetchall()
-
-genres = []
-
-for genre in genres_raw:
-  genres_named = namedtuple("genres_data", ["typename", "intro_text", "abbrev", "icon"])
-  genres.append(genres_named(*genre)._asdict())
+genres = data_dict(crsr.fetchall(), "genres_data", ["typename", "intro_text", "abbrev", "icon"])
 
 works = {}
 
-for genre_item in genres:
-  works[genre_item['abbrev']] = []
-  crsr.execute("SELECT WORKLIST.work_ID, WORKLIST.year, WORKLIST.period, CLIENTS.client_name, WORKLIST.title, WORKLIST.desc, WORKLIST.filename FROM WORKLIST, TYPES, CLIENTS WHERE TYPES.type_ID = WORKLIST.type AND CLIENTS.client_ID = WORKLIST.client AND TYPES.abbrev = (?) AND WORKLIST.show = 1 ORDER BY WORKLIST.year DESC", (genre_item['abbrev'],))
-  curworks = crsr.fetchall()
-  if len(curworks) !=0:
-    for current in curworks:
-      tmpline = []
-      tmpline += current
-      crsr.execute ("SELECT DETAILS.detail FROM DETAILS, WORKLIST WHERE DETAILS.to_work = WORKLIST.work_ID AND WORKLIST.work_id = (?) ORDER BY DETAILS.detail_ID", str(current[0]))
-      curdesc = crsr.fetchall()
-      if len(curdesc) !=0:
-        for descitem in curdesc:
-          tmpline += descitem
-      works[genre_item['abbrev']].append(tmpline)
+for curr_genre in genres:
+  curr_genre_abbrev = curr_genre['abbrev']
+  crsr.execute("SELECT WORKLIST.work_ID, WORKLIST.year, WORKLIST.period, CLIENTS.client_name, WORKLIST.title, WORKLIST.desc, WORKLIST.filename FROM WORKLIST, TYPES, CLIENTS WHERE TYPES.type_ID = WORKLIST.type AND CLIENTS.client_ID = WORKLIST.client AND TYPES.abbrev = (?) AND WORKLIST.show = 1 ORDER BY WORKLIST.year DESC", (curr_genre_abbrev,))
+  works[curr_genre_abbrev] = data_dict(crsr.fetchall(), "works_data", ["id", "year", "period", "cust", "name", "desc", "filename"])
+
+  for curr_work in works[curr_genre_abbrev]:
+    crsr.execute ("SELECT DETAILS.detail FROM DETAILS, WORKLIST WHERE DETAILS.to_work = WORKLIST.work_ID AND WORKLIST.work_id = (?) ORDER BY DETAILS.detail_ID", str(curr_work['id']))
+    curr_desc = crsr.fetchall()
+    desc_list = []
+    if len(curr_desc) != 0:
+      for desc_item in curr_desc:
+        desc_list += list(desc_item)
+    curr_work ['details'] = desc_list
 
 crsr.execute("SELECT title, keywords, description, quota, quota_author, copyright, about, contacts FROM TEXTS where line_ID = 1")
 line = crsr.fetchone()
-
 texts = namedtuple("site_texts", ["title", "keywords", "description", "quota", "quota_author", "copyright", "about", "contacts"])
 
 db.close()
